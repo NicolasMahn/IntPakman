abc85f531dffac2c4914dd68b5b9256f5dc96cb5
Leon Rottler <l.rottler@protonmail.com>
2021-11-10 22:17:27 +0100

    Erste ML Implementation

--- Machine_Learning_V1.py
+++ Machine_Learning_V1.py
@@ -1,12 +1,54 @@
+import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, roc_curve
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('data/paketdaten.csv', sep=';')
 
-fig = plt.figure()
-plt.scatter(data.id, data.gewicht_in_g)
-fig.show()
+def data_preparation(data):
+    data['volume'] = (data.length_cm * data.height_cm * data.width_cm)/1000
+    #data.drop(columns='id', inplace=True)
+    data['prio'] = np.where((data.volume > 3000) | (data.weight_in_g > 25000), 1, 0)
+    #data.loc[(data.volume > 3000) | (data.weight_in_g > 25000), ['prio']] = int(1)
+    #data['prio'] = data.[](1 if (data.volume > 3000) | (data.weight_in_g > 25000) else 0)]
 
-Xtrain, Xtest, ytrain, ytest = train_test_split(data.drop('', inplace=True), )
+    return data
+
+
+def data_details(data):
+    data.info()
+    data.weight_in_g.describe()
+    data.length_cm.describe()
+    data.height_cm.describe()
+    data.width_cm.describe()
+    data.volume.describe()
+
+
+def print_plot(data, column):
+    fig = plt.figure()
+    plt.scatter(data.id, data[column])
+    plt.title('Overview of ' + str(column))
+    fig.show()
+
+
+input_data = pd.read_csv('data/paketdaten.csv', sep=';', decimal=',')
+print_plot(input_data, 'weight_in_g')
+cleaned_data = data_preparation(input_data)
+print_plot(cleaned_data, 'volume')
+
+model = DecisionTreeClassifier()
+Xtrain, Xtest, ytrain, ytest = train_test_split(cleaned_data.drop(columns='prio'), cleaned_data.prio, test_size=0.2, random_state=42)
+
+model.fit(Xtrain, ytrain)
+pred = model.predict(Xtest)
+print(confusion_matrix(ytest, pred))
+
+pred_prob = model.predict_proba(Xtest)
+fpr, tpr, thresholds = roc_curve(ytest, pred_prob[:,1])
+plt.plot(fpr, tpr)
+plt.plot([0,1],[0,1])
+plt.title('ROC-Curve')
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.show()
