2caaf34b23a10ca0e8c456ea0c987339da817c5e
Leon Rottler <l.rottler@protonmail.com>
2021-11-26 00:57:53 +0100

    Added statement to retrieve all distances from neo4j

---
+++ Neo4j/GetDistances.py
@@ -0,0 +1,62 @@
+from neo4j import GraphDatabase
+
+
+class GetDistances:
+
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self.driver.close()
+
+    def neo_transaction_station_addresses(self):
+        with self.driver.session() as session:
+            result = session.write_transaction(
+                self._get_distance_station_addresses)
+
+            return result
+
+    @staticmethod
+    def _get_distance_station_addresses(tx):
+        query = 'MATCH (s:PostStation)-[r:DISTANCE_TO]->(a:Address) RETURN s.id,a.id,r.distance,r.duration'
+        result = tx.run(query)
+        print(result)
+        values = []
+        for record in result:
+            values.append(record.data())
+
+        return values
+
+
+    def neo_transaction_addresses_addresses(self):
+        with self.driver.session() as session:
+            result = session.write_transaction(
+                self._get_distance_addresses_addresses)
+
+            return result
+
+    @staticmethod
+    def _get_distance_addresses_addresses(tx):
+        query = 'MATCH (a1:Address)-[r:DISTANCE_TO]->(a2:Address) RETURN a1.id,a2.id,r.distance,r.duration'
+        result = tx.run(query)
+        print(result)
+        values = []
+        for record in result:
+            values.append(record.data())
+
+        return values
+
+
+
+def get_distance_station_addresses():
+    connector = GetDistances("bolt://192.52.37.239:7687", "neo4j", "test")
+    return connector.neo_transaction_station_addresses()
+
+def get_distance_addresses_addresses():
+    connector = GetDistances("bolt://192.52.37.239:7687", "neo4j", "test")
+    return connector.neo_transaction_addresses_addresses()
+
+def get_distance_between_all():
+    reslut_list = get_distance_station_addresses()
+    reslut_list.extend(get_distance_addresses_addresses())
+    return reslut_list
---
+++ TestScript.py
@@ -0,0 +1,65 @@
+import pickle
+from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
+import Neo4j.GetAddessesWithPackages as loader
+import Neo4j.GetPostStation as ploader
+import Neo4j.GetDistances as dloader
+
+'''
+model = DecisionTreeClassifier()
+
+path = 'Models/model_Classifier_without_volue'
+model = pickle.load(open(path, 'rb'))
+
+test = {"length_cm":["120"],
+        "width_cm":["98"],
+        "height_cm":["50"],
+        "weight_in_g":["2500"]
+}
+x = pd.DataFrame(test)
+result = model.predict(x)[0]
+'''
+
+distances = dloader.get_distance_station_addresses()
+print(distances)
+print(distances[0])
+print(len(distances))
+
+print('-------------------------------------------------------')
+
+daddresses = dloader.get_distance_addresses_addresses()
+print(daddresses)
+print(daddresses[0])
+print(len(daddresses))
+print(type(daddresses))
+
+print('-------------------------------------------------------')
+
+distances.extend(daddresses)
+print(distances)
+print(distances[len(distances)-1])
+print(len(distances))
+
+print('-------------------------------------------------------')
+
+completeList = dloader.get_distance_between_all()
+print(completeList)
+print(len(completeList))
+
+'''
+Example of one list element of the result_list
+{'a1.id': '5', 'a2.id': '19', 'r.distance': '750', 'r.duration': '148'}
+'''
+
+print('-------------------------------------------------------')
+
+'''
+addresses = loader.get_addresses_with_packages()
+station = ploader.get_post_station()
+print(addresses)
+print(len(addresses))
+print(station)
+
+addresses.append(station)
+print(addresses)
+'''
