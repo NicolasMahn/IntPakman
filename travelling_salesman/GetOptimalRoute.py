072b4c0c825ac3e7e2b16e5f027d21d4f92ba11e
Leon Rottler <l.rottler@protonmail.com>
2021-12-15 01:45:25 +0100

    Added Comments to the code

--- travelling_salesman/GetOptimalRoute.py
+++ travelling_salesman/GetOptimalRoute.py
@@ -17,6 +17,15 @@ def fix_values_in_list(input, number_of_knots):
 
 
 def create_key_dict(input, key_dict, key_dict_back):
+    """
+    necessary because the TSP function expects the knots to be numerated from 0 to how many knots are existing.
+    creates two dictionary. One has as the key the id of the address and as the value it has a new id calculated with
+    a counter. The second dict is the other way around, to be able to convert it back later.
+    :param input: data from db query
+    :param key_dict: empty, needs to be created
+    :param key_dict_back: empty, needs to be created
+    :return: inpunt (unchanged), created key_dict, created key_dict_back
+    """
     counter = 1
     for item in input:
         if 's.id' in item:
@@ -27,6 +36,13 @@ def create_key_dict(input, key_dict, key_dict_back):
 
 
 def change_keys(input, key_dict):
+    """
+    Changes the address id to a new id defined in the key_dict. Two different dict types in this case. Need to check
+    if the dict is from Post-Station to Address or from Address to Address
+    :param input: data from DB
+    :param key_dict: dict with the address id as input and the new id as value
+    :return:
+    """
     for item in input:
         if 's.id' in item:
             item['a.id'] = str(key_dict.get(item.get('a.id')))
@@ -37,6 +53,11 @@ def change_keys(input, key_dict):
 
 
 def create_final_list(input):
+    """
+    Gets the preprocessed list of dict-elements and returns list of list elements
+    :param input: data from DB with changed keys
+    :return: list containing list-elements like: [[knot1, knot 2, distance]]
+    """
     final_list = []
     for item in input:
         if 's.id' in item:
@@ -58,16 +79,12 @@ def change_keys_back(result, key_dict_back):
     return result
 
 
-def rearange_route(final_result):
-    index = [i for i, x in enumerate(final_result) if x == 0]
-    list_end = final_result[0:index[0]]
-    list_starting_with_zero = final_result[index[0]:len(final_result)]
-    list_starting_with_zero.extend(list_end)
-    route_list = list_starting_with_zero
-    return route_list
-
-
 def get_final_input_list_and_key_dict():
+    """
+    Gets data from DB and executes data preparation steps needed to transform input into readable list for tsp.
+    :return: list that contains list-elements with data like this: [[knot 1, knot 2, distance]], returns key_dict and
+    key_dict_back
+    """
     data = db_loader.get_distance_between_all()
 
     number_of_knots = 1
@@ -89,6 +106,12 @@ def change_keys_prio(input, key_dict):
 
 
 def clean_data_from_duplicate_addresses(input):
+    """
+    Removes duplicated addresses that recieve more than one package and returns prio 1 if any package has that prio,
+    otherwise return prio 0 for this address.
+    :param input: data from DB with changed keys
+    :return: list with dict-elements like {"a.id":id, "p.prio":prio}
+    """
     key_list = [item["a.id"] for item in input]
     unique_key_list = Counter(key_list).keys()
 
@@ -114,6 +137,11 @@ def clean_data_from_duplicate_addresses(input):
 
 
 def create_final_prio_list(input):
+    """
+    Gets the preprocessed list of dict-elements and returns list of list elements
+    :param input: data from DB with changed keys and removed duplicates
+    :return: list containing list-elements like: [[knot, prio]]
+    """
     final_prio_list = []
     for item in input:
         final_prio_list.append([int(item["a.id"]), int(item["p.prio"])])
@@ -122,6 +150,11 @@ def create_final_prio_list(input):
 
 
 def get_prio_list(key_dict):
+    """
+    Gets data from DB and executes data preparation steps needed to transform input into readable prio list for tsp.
+    :param key_dict: dict with address id as key and new id als value / created before for final_list
+    :return:list containing list-elements like: [[knot, prio]]
+    """
     data = db_loader_prio.get_prio_status_packages()
 
     data_with_changed_keys = change_keys_prio(data, key_dict)
@@ -130,7 +163,30 @@ def get_prio_list(key_dict):
     return final_prio_list
 
 
+def rearange_route(final_result):
+    """
+    Gets the result of the tsp algorithm with already changed keys and rearranges it, so the route always starts at the
+    Post-Station.
+    :param final_result: optimal route calculated by tsp algorithm
+    :return: list with the rearranged route, starting at the Post-Station
+    """
+    index = [i for i, x in enumerate(final_result) if x == 0]
+    list_end = final_result[0:index[0]]
+    list_starting_with_zero = final_result[index[0]:len(final_result)]
+    list_starting_with_zero.extend(list_end)
+    route_list = list_starting_with_zero
+    return route_list
+
+
 def process_result(best_state, best_fitness, key_dict_back):
+    """
+    Gets the result of the tsp algorithm and changes the keys back to the address ids from the DB. Rearranges the route
+    so it always starts at the Post-Station and gets the address information from the DB to match the route address id
+    with the address information from the DB. Prints final route.
+    :param best_state:
+    :param best_fitness:
+    :param key_dict_back:
+    """
     final_result = change_keys_back(best_state, key_dict_back)
     final_result_list = final_result.tolist()
     route = rearange_route(final_result_list)
